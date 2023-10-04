ba8596f7421e8c41c3f8a7f648ea1836a90d4b9a
xandejlm <alexandrej.lima.mendes@gmail.com>
2023-10-04 08:23:18 -0300

    removed unused functions

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function loadPokemon() {
   }
   
   let url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-    //o que vamos fazer com os dados em formato json
   fetch(url)
   .then((response) => {
     if(!response.ok){
@@ -19,7 +18,6 @@ function loadPokemon() {
     console.clear();
     console.log(data);
     document.getElementById('name').innerHTML = data['name'];
-    // document.getElementById('type').innerHTML = data['types'];
     document.getElementById('type').innerHTML = '';
     data.types.forEach((type)=>{
       const typeElement = document.createElement('span');
@@ -29,68 +27,11 @@ function loadPokemon() {
 
     let img = data['sprites']['front_default'];
     document.getElementById('pic').setAttribute('src', img);
-
-    let pokemonId = data.id;
-    getEvolutions(pokemonId);
   })
   .catch(erro => {
     console.log("Erro " + erro);
     alert('Pokemon não encontrado.');
   });
-}
-
-
-function getNamesEvolutions(evolutionChain) {
-  const evolutionNames = [];
-  
-  const traverse = (evolution) => {
-    evolutionNames.push(evolution.species.name);
-    if (evolution.evolves_to.length > 0) {
-      evolution.evolves_to.forEach((nextEvolution) => {
-        traverse(nextEvolution);
-      });
-    }
-  };
-
-  traverse(evolutionChain);
-
-  return evolutionNames;
-}
-
-
-function getEvolutions(pokemonId) {
-  let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
-  fetch(pokemonURL)
-    .then((response) => {
-      if (!response.ok) throw new Error('Não foi possível encontrar o Pokémon.');
-      return response.json();
-    })
-    .then((pokemonData) => {
-      let speciesURL = pokemonData.species.url;
-      return fetch(speciesURL);
-    })
-    .then((response) => {
-      if (!response.ok) throw new Error('Não foi possível encontrar informações da espécie.');
-      return response.json();
-    })
-    .then((speciesData) => {
-      let evolutionChainURL = speciesData.evolution_chain.url;
-      return fetch(evolutionChainURL);
-    })
-    .then((response) => {
-      if (!response.ok) throw new Error('Não foi possível encontrar informações da cadeia de evolução.');
-      return response.json();
-    })
-    .then((evolutionData) => {
-      console.log(evolutionData);
-      // printEvolutions(evolutionData.chain);
-      let array = getNamesEvolutions(evolutionData.chain);
-      console.log(array);
-    })
-    .catch((error) => {
-      console.error('Erro: ' + error);
-      alert('Erro ao obter informações de evolução.');
-    });
 }
 
 document.getElementById('btn_load_pokemon').onclick = loadPokemon;
